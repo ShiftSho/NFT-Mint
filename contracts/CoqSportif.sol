06fc1414f7996628bb206a676d77ba19e1467dca
ShiftSho <jlongprivate@gmail.com>
2024-01-24 15:00:33 -0500

    Initial commit

--- contracts/CoqSportif.sol
+++ contracts/CoqSportif.sol
@@ -6,18 +6,22 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, ERC721URIStorage, Ownable {
+contract CoqSportif is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("MyToken", "MTK") {}
+    constructor() ERC721("CoqSportif", "CSF") {}
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
     // The following functions are overrides required by Solidity.
--- ipfs/upload.js
+++ ipfs/upload.js
@@ -1,28 +1,31 @@
+import { createHelia } from "helia";
+import { dagJson } from "@helia/dag-json";
+
 async function run() {
-    const { create } = await import('ipfs-http-client');
-    const ipfs = await create();
+    const helia = await createHelia();
+    const ipfs = dagJson(helia);
     
     // we added three attributes, add as many as you want!
     const metadata = {
         path: '/',
         content: JSON.stringify({
-            name: "My First NFT",
+            name: "Meme Sportif",
             attributes: [
             {
-                "trait_type": "Peace",
-                "value": "10" 
+                "trait_type": "Pills",
+                "value": "Basic Viagra" 
             },
             {
-                "trait_type": "Love",
-                "value": "100"
+                "trait_type": "Clothes",
+                "value": "Le Coq Sportif Tracksuit"
             },
             {
-                "trait_type": "Web3",
-                "value": "1000"
+                "trait_type": "Glasses",
+                "value": "Sunglasses"
             }
             ],
             // update the IPFS CID to be your image CID
-            image: "https://ipfs.io/ipfs/QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p",
+            image: "https://ipfs.io/ipfs/QmNRMzUuLUimrX2jzA9pij92Uo9XGBJc8TiqY7wLmUp2tB",
             description: "So much PLW3!"
         })
     };
